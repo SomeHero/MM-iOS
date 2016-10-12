1f461d627df8d50d7be349dabfbf544f6782bcb2
James Rhodes <james@somehero.com>
2016-10-12 15:49:01 -0400

    set prod/staging environment constant

--- MemberMoose-iOS/Constants.swift
+++ MemberMoose-iOS/Constants.swift
@@ -17,7 +17,7 @@ let kCognitoPoolID = "us-east-1:0f789e2a-95db-450f-8698-322a99c2cfe1"
 let kS3BucketName = "ishopaway-staging"
 let kS3ImageBaseUrl = "https://s3.amazonaws.com/ishopaway-staging/"
 #else
-let kApiBaseUrl = "https://membermoose-ng.herokuapp.com/api/"
+let kApiBaseUrl = "https://membermoose-node.herokuapp.com/api/"
 let kOnePX: CGFloat = 1.0 / UIScreen.mainScreen().scale
 let kStripePublishableKey = "pk_test_biSyfSYHVZjhj0lVfqep9HOA"
 let kGoogleTrackingId = "UA-78467459-3"
